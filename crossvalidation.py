c19aaf96e9f2c711a6a86465cc6c3d724dbfaa60
Ulyannah <mmracool@gmail.com>
2020-10-26 14:01:01 +0300

    added crossvalidation

---
+++ crossvalidation.py
@@ -0,0 +1,16 @@
+from sklearn import preprocessing
+import parsing as ps
+import test as ts
+from sklearn.linear_model import LogisticRegressionCV
+import logit_reg as lg
+print(lg.Y_women)
+data_to_test = ps.data_to_test
+data_to_ml = ps.data_to_ml
+columns = ts.columns_to_ml
+
+
+clf = LogisticRegressionCV(cv=5, random_state=1).fit(preprocessing.scale(lg.X_men), lg.Y_men)
+clf.predict(preprocessing.scale(lg.X_women)[:2, :])
+clf.predict_proba(preprocessing.scale(lg.X_women)[:2, :]).shape
+
+print(clf.score(preprocessing.scale(lg.X_women), lg.Y_women))
